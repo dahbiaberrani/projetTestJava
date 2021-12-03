0275d16dd8da2d28c4fb6d6241ed8572c202743d
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2021-12-03 19:20:01 +0100

    le main compile sans la partie PassagerStandard
    Signed-off-by: Dahbia BERRANI <dehbia.berrani1993@gmail.com>

--- partie3/src/test/java/tec/EtatPassagerTest.java
+++ partie3/src/test/java/tec/EtatPassagerTest.java
@@ -1,12 +1,13 @@
 package tec;
 
-//import org.junit.Test;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tec.EtatPassager.Etat.*;
 
 class EtatPassagerTest {
