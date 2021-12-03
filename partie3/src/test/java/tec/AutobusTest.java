77d84c83a741983f5009a868767107a294145c98
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2021-12-03 18:10:58 +0100

    documentation autobusTest
    Signed-off-by: Dahbia BERRANI <dehbia.berrani1993@gmail.com>

--- partie3/src/test/java/tec/AutobusTest.java
+++ partie3/src/test/java/tec/AutobusTest.java
@@ -7,52 +7,76 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AutobusTest {
 
-//    Autobus autobus1,autobus2;
-//    Passager diane, lila;
-    @BeforeEach
-    void setUp() {
-//         autobus1 = new Autobus(5,0);
-//         autobus2= new Autobus(0,8);
-
-
-
-    }
-
 
     /**
-     * en test si y a
-     * @return vrai s'il existe des places assises
+     * Avec une instanciation dans autobus:
+     * <p> A vérifier :
+     * 	 <p>aPlaceAssise retourne false si il n' y a pas des place  assises dans autobus.
+     * 	 <p>aPlaceAssise retourne vrai si il y a des place assises dans  autobus.
      */
+
     @Test
     void aPlaceAssise() {
 
-//        assertFalse(true, autobus1.aPlaceAssise());
-//        assertTrue(true, autobus2.aPlaceAssise());
     }
+
+    /**
+     * Avec une instanciation de autobus:
+     * <p> A vérifier :
+     * 	 <p>aPlaceDebout retourne false si il n' y a pas des place  Debout dans autobus.
+     * 	 <p>aPlaceDebout retourne vrai si il y a des place  Debout dans  autobus.
+     */
 
     @Test
     void aPlaceDebout() {
-//        assertTrue(true, autobus1.aPlaceAssise());
     }
 
+    /**Avec une instanciation de passager
+     * <p> A vérifier:
+     * <p> retourne vrai si l'etat passager est passer de Dehors à assise.
+     */
+
     @Test
-    void demanderPlaceAssise() {
-//        assertFalse(true, autobus2.aPlaceAssise());
-//        assertTrue(true, autobus1.aPlaceAssise());
+    void demanderPlaceAssise(Passager p) {
+
     }
+
+    /**Avec une instanciation de passager
+     * <p> A vérifier:
+     * <p> retourne vrai si l'etat passager est passer de Dehors à debout.
+     */
 
     @Test
     void demanderPlaceDebout() {
 
     }
 
+    /**Avec une instanciation de passager
+     * <p> A vérifier:
+     * <p> retourne vrai si l'etat passager est passer de assise à debout.
+     * <p> retourne false si l'état de passager ne change pas de assise à l'état debout .
+     */
+
     @Test
     void demanderChangerEnDebout() {
     }
 
+    /**Avec une instanciation de passager
+     * <p> A vérifier:
+     * <p> retourne vrai si l'etat passager est passer de debout à assise.
+     * <p> retourne false si l'état de passager ne change pas de debout à l'état assise .
+     */
+
     @Test
     void demanderChangerEnAssis() {
     }
+
+    /**Avec une instanciation de passager
+     * <p> A vérifier:
+     * <p> retourne vrai si l'etat passager est passer de debout à dehors.
+     * <p> retourne vrai si l'etat passager est passer de assise à dehors.
+     * <p> retourne false si l'état de passager ne change pas de assise ou debout à l'état dehors
+     */
 
     @Test
     void demanderSortie() {
