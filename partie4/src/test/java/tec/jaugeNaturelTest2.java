356a96cb11854be7d2ccac95db615ecc8f869082
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2021-12-17 11:52:18 +0100

    modifier testJaugeNaturelTest2
    Signed-off-by: Dahbia BERRANI <dehbia.berrani1993@gmail.com>

--- partie4/src/test/java/tec/jaugeNaturelTest2.java
+++ partie4/src/test/java/tec/jaugeNaturelTest2.java
@@ -8,7 +8,7 @@ import tec.jaugeNaturel;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class jaugeNaturelTest2 {
-    jaugeNaturel jauge1, jauge2, jauge3, jauge4;
+    jaugeNaturel jauge1, jauge2, jauge3, jauge4,jauge5,jaugeEgal,jaugeSup,jaugeSupInter;
 
     @BeforeEach
     void setUp() {
@@ -16,6 +16,10 @@ public class jaugeNaturelTest2 {
         jauge2 = new jaugeNaturel(1, 100, 50);
         jauge3 = new jaugeNaturel(1, 100, 1);
         jauge4 = new jaugeNaturel(1, 100, 0);
+        jauge5 = new jaugeNaturel(50, 25, 30);
+        jaugeEgal = new jaugeNaturel(20, 20, 20);
+        jaugeSup = new jaugeNaturel(20, 20, 30);
+        jaugeSupInter = new jaugeNaturel(1, 40, 41);
 
     }
 
@@ -25,6 +29,10 @@ public class jaugeNaturelTest2 {
         jauge2 = null;
         jauge3 = null;
         jauge4 = null;
+        jauge5 = null;
+        jaugeEgal = null;
+        jaugeSup = null;
+        jaugeSupInter = null;
     }
 
     @Test
@@ -33,29 +41,47 @@ public class jaugeNaturelTest2 {
         assertTrue(jauge2.estVert(), "estVert() devrait renvoyer true");
         assertFalse(jauge2.estBleu(), "estBleu() devrait renvoyer false");
     }
+
     @Test
-    void testDeplacer(){
-        assertTrue(jauge3.estBleu(),"estBleu() devrait renvoyer true");
+    void testDeplacer() {
+        assertTrue(jauge3.estBleu(), "estBleu() devrait renvoyer true");
         assertFalse(jauge3.estRouge(), "estRouge() devrait renvoyer false");
         assertFalse(jauge3.estVert(), "estVert() devrait renvoyer false");
         jauge3.incrementer();
-        assertFalse(jauge3.estBleu(),"estBleu() devrait renvoyer false");
+        assertFalse(jauge3.estBleu(), "estBleu() devrait renvoyer false");
         assertFalse(jauge3.estRouge(), "estRouge() devrait renvoyer false");
         assertTrue(jauge3.estVert(), "estVert() devrait renvoyer true");
     }
+
     @Test
-    void testInferieurIntervalle(){
-        assertTrue(jauge3.estBleu(),"estBleu() devrait renvoyer true");
+    void testInferieurIntervalle() {
+        assertTrue(jauge3.estBleu(), "estBleu() devrait renvoyer true");
         assertFalse(jauge3.estRouge(), "estRouge() devrait renvoyer false");
         assertFalse(jauge3.estVert(), "estVert() devrait renvoyer false");
 
-        assertTrue(jauge4.estBleu(),"estBleu() devrait renvoyer true");
+        assertTrue(jauge4.estBleu(), "estBleu() devrait renvoyer true");
         assertFalse(jauge4.estRouge(), "estRouge() devrait renvoyer false");
         assertFalse(jauge4.estVert(), "estVert() devrait renvoyer false");
 
     }
+
     @Test
-    void testSuperieurIntervalle(){
+    public void testLimiteVigieMaxInferieurVigieMin() {
+        assertTrue(jauge5.estBleu() && jauge5.estRouge() && !jauge5.estVert(), "La vigie max est inferieure a la vigie min");
+    }
+
+    @Test
+    public void testMaxEgaleMin(){
+        assertTrue(jaugeSup.estRouge(), "Vigie max égal à vigie min < départ");
+        assertTrue(jaugeEgal.estBleu() && jaugeEgal.estRouge(), "Vigie max égal à vigie min égal au départ");
+
+    }
+
+    @Test
+    void testSuperieurIntervalle() {
+        assertTrue(jaugeSupInter.estRouge(), "estRouge() devrait renvoyer true");
+        jaugeSupInter.decrementer();
+        assertTrue(jaugeSupInter.estRouge(), "estRouge() devrait renvoyer true");
 
     }
 }
